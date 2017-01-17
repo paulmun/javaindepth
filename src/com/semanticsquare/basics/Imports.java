6a2a6adccca91176644783b4fa63ff014243196b
Paul Mun <paulmun818@gmail.com>
2017-01-16 17:11:32 -0800

    Imports

---
+++ src/com/semanticsquare/basics/Imports.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+import java.util.ArrayList;
+
+
+class Imports {
+
+    public static void main(String[] args) {
+        Date date;
+        ArrayList list;
+        java.sql.Date date2;
+    }
+
+}
